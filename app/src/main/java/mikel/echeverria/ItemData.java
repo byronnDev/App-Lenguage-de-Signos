0b5fad308fcd9e4a9a37d51d903a52b26413cba4
byronnDev <38868773+byronnDev@users.noreply.github.com>
2023-10-27 13:25:14 +0200

    Code

---
+++ app/src/main/java/mikel/echeverria/ItemData.java
@@ -0,0 +1,37 @@
+package mikel.echeverria;
+
+public class ItemData {
+    private int imageResource;
+    private String name;
+    private String description;
+    private int color;
+    private int colorClaro;
+
+    public ItemData(int imageResource, String name, String description, int color, int colorClaro) {
+        this.imageResource = imageResource;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.colorClaro = colorClaro;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public int getColorClaro() {
+        return colorClaro;
+    }
+}
---
+++ app/src/main/java/mikel/echeverria/MainActivity2.java
@@ -0,0 +1,36 @@
+package mikel.echeverria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    ImageView imgImagen;
+    TextView txtName;
+    TextView txtDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        imgImagen = (ImageView) findViewById(R.id.imgImagen);
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtDescription = (TextView) findViewById(R.id.txtDescription);
+
+        // Recogemos la informacion pasada
+        Intent intent = getIntent();
+        int imageResource = intent.getIntExtra("imageResource", R.drawable.image1 /* default image */);
+        String name = intent.getStringExtra("name");
+        String description = intent.getStringExtra("description");
+
+        // Asignamos imagen, nombre y descripción
+        imgImagen.setImageResource(imageResource);
+        txtName.setText(name);
+        txtDescription.setText(description);
+
+    }
+}
