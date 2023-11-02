f1d671bbfee73a5f8f89789079d1c4e6fc1a1948
aitor <aitor@DESKTOP-GIR2RBG>
2023-11-02 13:59:30 +0100

    My aplication y metodo para introducir datos

--- app/src/main/java/mikel/echeverria/activities/MainActivity.java
+++ app/src/main/java/mikel/echeverria/activities/MainActivity.java
@@ -12,11 +12,15 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
 import mikel.echeverria.model.Card;
 import mikel.echeverria.R;
 import mikel.echeverria.adapters.RecyclerDataAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    Realm realm;
+    RealmResults<Card> listaDeElementos;
     RecyclerView recyclerView;
     RecyclerDataAdapter recyclerDataAdapter;
 
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        realm = Realm.getDefaultInstance();
         // Inicializa tu RecyclerView
         recyclerView = findViewById(R.id.my_recycler_view);
 
@@ -45,4 +50,31 @@ public class MainActivity extends AppCompatActivity {
         // Establece el adaptador en el RecyclerView
         recyclerView.setAdapter(recyclerDataAdapter);
     }
+
+
+
+
+
+    public void introducirDatos() {
+
+        if (listaDeElementos.isEmpty()){
+
+            listaDeElementos = realm.where(Card.class).findAll();
+            realm.commitTransaction();
+
+            //creacion de objetos
+            //elemento mago = new elemento("mago","El mago de la corte real, tambien engargado de ayudar al rey con su gran sabiduria",R.drawable._02_wizard,"#c39bd3", "#9b59b6");
+
+
+            realm.beginTransaction();
+            // subirlos a la base de datos
+            //realm.copyToRealm(caballero);
+            //realm.copyToRealm(mago);
+
+        }
+        listaDeElementos = realm.where(Card.class).findAll();
+        realm.commitTransaction();
+    }
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,11 @@ buildscript{
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath ("io.realm:realm-gradle-plugin:10.15.1")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.1" apply false
 }
