d34918361cf5d85fb49a5c4c8e23a8dc6ec325a5
byronnDev <mikel.e.g.2004@gmail.com>
2023-11-16 14:16:06 +0100

    Vistas modificadas, solucionada la posición y second activity arreglado

--- app/src/main/java/mikel/echeverria/activities/SecondActivity.java
+++ app/src/main/java/mikel/echeverria/activities/SecondActivity.java
@@ -1,46 +1,33 @@
 package mikel.echeverria.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.List;
-
 import io.realm.Realm;
-import io.realm.RealmQuery;
-import io.realm.RealmResults;
 import mikel.echeverria.R;
 import mikel.echeverria.model.Card;
 
 public class SecondActivity extends AppCompatActivity {
     ImageView imgImagen;
-    TextView txtName;
-
-    TextView txtFrases;
-    List<String> frases;
+    TextView txtName, txtFrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        Realm.init(this);  // Inicializa Realm en la app
 
-        imgImagen = (ImageView) findViewById(R.id.imgImagen);
-        txtName = (TextView) findViewById(R.id.txtName);
-        txtFrases = (TextView) findViewById(R.id.txtFrases);
+        imgImagen = findViewById(R.id.imgImagen);
+        txtName = findViewById(R.id.txtName);
+        txtFrases = findViewById(R.id.txtFrases);
 
-        // Recogemos la informacion pasada
-        Intent intent = getIntent();
-        int idCard = intent.getIntExtra("id", 0);
+        int idCard = getIntent().getIntExtra("id", 0);
 
-        Realm realm = Realm.getDefaultInstance(); // Obtengo una instancia de Realm
-        Card card = realm.where(Card.class).equalTo("idCard", idCard).findFirst(); // Obtengo el primer objeto de tipo Card con el idCard igual al idCard pasado del activity anterior
+        Realm realm = Realm.getDefaultInstance();
+        Card card = realm.where(Card.class).equalTo("id", idCard).findFirst();
         realm.close();
 
-        // Asignamos imagen, nombre y descripción
         imgImagen.setImageResource(card.getImageResource());
         txtName.setText(card.getName());
         for (String frase: card.getFrases()) {
